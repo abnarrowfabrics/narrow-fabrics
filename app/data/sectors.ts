cc224ac197a260199407fbbe6a6af9afd486516c
webthism <webthism@gmail.com>
2026-08-12 12:05:38 +0530

    removed the medial from market sector

--- app/data/sectors.ts
+++ app/data/sectors.ts
@@ -1,10 +1,5 @@
 export const sectors = [
   {
-    name: "Medical",
-    description:
-      "Neelam Lace has supplied webbing to the medical industry for more than 15 years.",
-  },
-  {
     name: "Promotional",
     description: "Promotional webbings for branding, messages & safety instructions.",
   },
